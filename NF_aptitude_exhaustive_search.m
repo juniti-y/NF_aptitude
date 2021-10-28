966ccf29760894abbd35072c86983dbfbc906eca
Junichiro Yoshimoto <juniti-y@is.naist.jp>
2021-10-28 10:39:37 +0900

    Add files via upload
    Add some comments

--- NF_aptitude_exhaustive_search.m
+++ NF_aptitude_exhaustive_search.m
@@ -1,8 +1,21 @@
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% When applying this script to your own data, please replace the following
+% variables between lines 18-20:
+%  * FC_all: N-by-D dim. Functional connectivity data matrix, where the
+%    rows are N study participants and the columns are the values of D
+%    distinct FC.
+%  * NFscore_all: N-dim. column vector containing the neurofeedback scores
+%    of N study participants
+%  * mask_train: N-dim. column vector containing dummy variables, each of
+%    which indicates whether the participant should be used for the training
+%    data (indicated by "true") or the test data (indicated by "false").
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clear
 close all
 
-%generation of dummy data 
-FC_all=rand(65, 253)*2-1; %functional connectivity
+%generation of dummy data (Please replace them by your own data.)
+FC_all=rand(65, 4)*2-1; %functional connectivity
 NFscore_all=rand(65, 1); %NF aptitude score
 mask_train=[true(41,1); false(24,1)]; %41 training data and 24 test data
 
@@ -12,13 +25,16 @@ N_train = length(Y_train); %number of training data (discovery dataset)
 X_test = FC_all(~mask_train,:); %explanatory variable of test data (independent dataset)
 Y_test = NFscore_all(~mask_train); %objective variable of test data (independent dataset)
 
-var_idx = cell(27000000,1));
+% Get the total number of FCs
+Tot_Num_FC = size(FC_all, 2);
+
+var_idx = cell(27000000,1);
 idx = 0;
-for p = 1:28 %8 choose 2
-    tmp = combnk(1:28,p);
+for p = 1:Tot_Num_FC %8 choose 2
+    tmp = combnk(1:Tot_Num_FC,p);
     for k = 1:size(tmp,1)
         idx = idx+1;
-        var_idx{idx} = tmp(k,:); %sets of FCs from all 28 FCs
+        var_idx{idx} = tmp(k,:); %sets of FCs from all FCs
     end
 end
 var_idx(idx+1:end) = []; 
@@ -28,7 +44,7 @@ for k = 1:MaxK
 	if mod(k, 1000000)==0
 	disp([k,MaxK]) %show progress
 	end
-    mask_var = false(1,28);
+    mask_var = false(1,Tot_Num_FC);
     mask_var(var_idx{k}) = true;
     Y_pred = nan(size(Y_train));
 
@@ -67,8 +83,10 @@ end
 
 %scatter plot
 figure;
+
+co = get(gca,'colororder');
 %clf(fig1);
-scatter(Y_train, Y_pred, 100,co(1,:),'filled');
+scatter(Y_train, Y_pred, 100, co(1,:),'filled');
 hold on;
 mdl = polyfit(Y_train, Y_pred,1);
 xrng = get(gca,'XLim');
